a833f95d6ca1131f852c40a09b38d0c6dafe1b22
Mark Otuya <markotuya0@gmail.com>
2022-08-11 02:47:36 +0100

    centered the calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
   render() {
     return (
       <div className="calculator">
