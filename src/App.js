c9b9cfb5ee79953e24f6f8fe80b83054ede13133
Mark Otuya <markotuya0@gmail.com>
2022-08-19 00:56:34 +0100

    added quotes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import './App.css';
 import Calculator from './components/calculator';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Calculator />
+      <>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Header" element={<Header />} />
+            <Route path="/quote" element={<Quotes />} />
+          </Routes>
+        </Router>
+        <Calculator />
+      </>
+
     );
   }
 }
